44b5cf120beb3fa8ae8d3cb1a6c9c56df899ba95
Peter Petrik <zilolv@gmail.com>
2018-11-16 20:59:26 +0100

    missing folder

--- scripts/install_brew.bash
+++ scripts/install_brew.bash
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 read -p "Are you sure to install brew? " -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
@@ -32,11 +34,9 @@ brew install exiv2
 # does not support netcdf
 brew install osgeo/osgeo4mac/gdal2 --with-complete --with-libkml
 brew install osgeo/osgeo4mac/saga-gis-lts
-brew install grass7
 brew install gdal2-python
 
 # use this link until postgis is build on osgeo
-brew uninstall gdal # safety
 brew link gdal2 --force
 brew install https://raw.githubusercontent.com/OSGeo/homebrew-osgeo4mac/530a838c9d93721d0c2d5eee2ddeb702b848184f/Formula/postgis.rb --build-from-source
 
@@ -44,11 +44,89 @@ brew install https://raw.githubusercontent.com/OSGeo/homebrew-osgeo4mac/530a838c
 # tools
 pip3 install dropbox
 pip3 install GitPython
-
 pip3 install owslib
 
 # qgis deps
-brew install qgis3 --only-dependencies
+# brew install osgeo/osgeo4mac/qgis3 --only-dependencies
+# qgis install it brings homebrew-core/gdal dependency back
+brew install cmake
+brew install ninja
+brew install gsl
+brew install sip
+brew install bison
+brew install flex
+brew install pkg-config
+brew install python
+brew install qt
+brew install pyqt
+brew install pyqt5-webkit
+brew install x11
+brew install qca
+brew install qtkeychain
+brew install qscintilla2
+brew install qwt
+brew install qwtpolar
+brew install qjson
+brew install sqlite
+brew install expat
+brew install proj
+brew install spatialindex
+brew install postgresql
+brew install libpq
+brew install curl
+brew install libzip
+brew install libtasn1
+brew install hicolor-icon-theme
+brew install libiconv
+brew install geos
+brew install libspatialite
+brew install postgis
+brew install openssl
+brew install poppler
+brew install gnu-sed
+
+# matplotlib
+brew install numpy
+brew install brewsci/bio/matplotlib
+
+# core providers
+brew install gdal2-python
+
+brew install oracle-client-sdk
+brew install grass7
+brew install gettext
+brew install gpsbabel
+brew install pyspatialite
+brew install r
+brew install saga-gis-lts
+
+pip3 install pyqgis-startup
+pip3 installcertifi
+pip3 installchardet
+pip3 installidna
+pip3 installOWSLib
+pip3 installcython
+pip3 installpyproj
+pip3 installpython-dateutil
+pip3 installpytz
+pip3 installrequests
+pip3 installsix
+pip3 installurllib3
+pip3 installcoverage
+pip3 install funcsigs
+pip3 install future
+pip3 install mock
+pip3 install nose2
+pip3 install pbr
+pip3 install psycopg2
+pip3 install PyYAML
+pip3 install Jinja2
+pip3 install MarkupSafe
+pip3 install Pygments
+pip3 install termcolor
+pip3 install oauthlib
+pip3 install pyOpenSSL
+pip3 install numpy
 
 # extra python stuff
 
